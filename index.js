28ef791a97404c9121a75347e82f73e39531af5c
unknown <tonyhuang4326@gmail.com>
2021-03-05 16:05:54 -0800

    made a clear_data function and did all buttons

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-function displayItems(ar){
-    
-    document.getElementsByClassName("product-container")[0].innerHTML = "";
-
-    for(let i = 0; i < ar.length; i++){
-
-        var div = document.createElement("div");
-
-        var name = document.createElement("p");
-        var text = document.createTextNode(ar[i].name);
-        name.appendChild(text);
-
-        var price = document.createElement("p");
-        var cost = document.createTextNode("$" + ar[i].price);
-        price.appendChild(cost);
-
-        var link = document.createElement("a");
-        var url = document.createTextNode(ar[i].url);
-        link.appendChild(url);
-        link.href = ar[i].url;
-
-        div.appendChild(name);
-        div.appendChild(price);
-        div.appendChild(link);
-
-        document.getElementsByClassName("product-container")[0].appendChild(div);
-    }
-}
-
 // hardcoded some functions for now
 function display_product_information(product, i) {
     // ideally whenever a button is clicked we called the load json data function and then display the image
@@ -36,13 +7,18 @@ function display_product_information(product, i) {
 
     var img = document.createElement('img');
     var text = document.createElement('P');
-    text.innerText = `PRODUCER: ${product[i].name}      | PRICE: $${product[i].price}       | URL: ${product[i].url}`;
+    text.innerText = `PRODUCER: ${product[i].name}      | PRICE: $${product[i].price}       | URL: `;
+    var link = document.createElement("a");
+    link.innerText = product[i].url;
+    link.href = product[i].url;
+    text.appendChild(link);
     img.src = product[i].img;
     document.querySelector(".product-container").appendChild(img);
     document.querySelector(".product-container").appendChild(text);
 }
 
 function load_json_bed_data() {
+    clear_data();
     $.getJSON("bed.json", function(data) {
         for(var i = 0; i < data.bed.length; i++) {
             display_product_information(data.bed, i)
@@ -51,6 +27,7 @@ function load_json_bed_data() {
 }
 
 function load_json_bookcases_data() {
+    clear_data();
     $.getJSON("bookcase.json", function(data) {
         for(var i = 0; i < data.bookcase.length; i++) {
             display_product_information(data.bookcase, i)
@@ -59,6 +36,7 @@ function load_json_bookcases_data() {
 }
 
 function load_json_chair_data() {
+    clear_data();
     $.getJSON("chair.json", function(data) {
         for(var i = 0; i < data.chair.length; i++) {
             display_product_information(data.chair, i)
@@ -67,6 +45,7 @@ function load_json_chair_data() {
 }
 
 function load_json_desk_data() {
+    clear_data();
     $.getJSON("desk.json", function(data) {
         for(var i = 0; i < data.desk.length; i++) {
             display_product_information(data.desk, i)
@@ -75,6 +54,7 @@ function load_json_desk_data() {
 }
 
 function load_json_dresser_data() {
+    clear_data();
     $.getJSON("dresser.json", function(data) {
         for(var i = 0; i < data.dresser.length; i++) {
             display_product_information(data.dresser, i)
@@ -86,4 +66,6 @@ function load_json_dresser_data() {
 
 // here we want to have functions to show json data when a button is pressed
 
-
+function clear_data(){
+    document.querySelector(".product-container").innerHTML = "";
+}
